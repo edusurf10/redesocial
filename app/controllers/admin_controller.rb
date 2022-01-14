8815fa8530b8ea8a1c4acf7e03cbdc7524fbe14a
Luiz Eduardo <tutoriais.gamehelp@gmail.com>
2022-01-14 09:11:26 -0300

    devise admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,17 @@
+class AdminController < ApplicationController
+  before_action :authenticate_admin
+
+  def authenticate_admin
+    redirect_to new_admin_session_path unless current_admin?
+  end
+
+  def current_admin?
+    if current_admin.present?
+      @admin = current_admin
+      true
+    elsif
+      false
+    end
+  end
+
+end
---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  it "Admin is valid" do
+    admin = Admin.new(email: "admin@admin.com", password: "123456") #criar instancia admin
+
+    expect(admin.email).to eql "admin@admin.com"
+    expect(admin).to be_valid
+  end
+
+  it  "Admin is not valid" do
+    admin = Admin.new(email: "admin@admin.com") #criar instancia admin
+
+    expect(admin).to_not be_valid
+  end
+end
